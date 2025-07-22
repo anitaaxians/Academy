fb4384cb62759969d76b1136854054a06d13adaf
anitaaxians <anitaaxians@gmail.com>
2025-07-22 15:51:46 +0200

    Added Login&Register DTOs

---
+++ Academy/DTOs/RegisterRequestDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Academy.DTOs
+{
+    public class RegisterRequestDto
+    {
+        [Required]
+        public string FullName { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public DateTime Birthday { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+}
--- Academy/Program.cs
+++ Academy/Program.cs
@@ -1,3 +1,8 @@
+using Academy.Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 namespace Academy
 {
@@ -13,19 +18,39 @@ namespace Academy
             // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
             builder.Services.AddOpenApi();
 
+            //Sql Server
+            builder.Services.AddDbContext<AppDbContext>(o => o.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+
+            builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(
+                        Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]!))
+                };
+            });
+
+
+
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
                 app.MapOpenApi();
-            } 
+            }
 
             app.UseHttpsRedirection();
 
             app.UseAuthorization();
 
-           
+
 
             app.Run();
         }
