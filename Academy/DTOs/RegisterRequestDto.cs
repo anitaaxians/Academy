f52e4633addf9476f44a9caf549469a7c002a38c
anitaaxians <anitaaxians@gmail.com>
2025-07-29 13:22:03 +0200

    Code formating

--- Academy/DTOs/RegisterRequestDto.cs
+++ Academy/DTOs/RegisterRequestDto.cs
@@ -4,9 +4,14 @@ namespace Academy.DTOs
 {
     public class RegisterRequestDto
     {
+        [Required]
         public string Fullname { get; set; } = string.Empty;
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
+        [Required]
         public DateTime Birthday { get; set; }
+        [Required]
         public string Password { get; set; }
     }
 }
